f8631322c6c6afec3d0345a2e659fca867d7667d
abdullahaameen <abdullah.aameen@dxc.com>
2024-02-14 10:03:49 +0530

    Commit for Login Page

---
+++ LoginApp/LoginApp/LoginApp/MyPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+ 
+using Xamarin.Forms;
+ 
+namespace XamarinLoginApp
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            string username = usernameEntry.Text;
+            string password = passwordEntry.Text;
+
+            // Hardcoded username and password
+            string validUsername = "admin";
+            string validPassword = "admin";
+
+            if (username == validUsername && password == validPassword)
+            {
+                // Display login success alert
+                DisplayAlert("Login Success", "", "OK");
+            }
+            else
+            {
+                // Display login failed alert
+                DisplayAlert("Login Failed", "", "Try Again");
+            }
+        }
+
+    }
+}
